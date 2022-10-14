fb409c4bdd51a864fd0bd67697f7f3fb4a132bfc
Shaima <shaima.anvar@gmail.com>
2022-10-13 20:26:36 -0400

    React-5 class components

---
+++ src/Components/ClassComp.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+class ClassComp extends Component {
+    constructor(){
+        super();
+        this.state={count:10}
+    }
+    handleIncrement = ()=>this.setState({count:this.state.count+5})
+    
+    handleDecrement = ()=>this.setState({count:this.state.count-1})
+    
+    componentDidMount(){
+        console.log('Component mounted');
+    }
+  render() {
+    return (
+      <div>
+        <h2>Counter: {this.state.count}</h2>
+        <button onClick={this.handleIncrement}>Increment</button>
+        <button onClick={this.handleDecrement}>Decrement</button>
+      </div>
+    )
+  }
+}
+export default ClassComp
